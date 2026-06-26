745c7283068318016bbe1bb480b2f07263f00b74
Talha-Faheem <talhafaheem1103@gmail.com>
2026-06-26 21:15:28 +0500

    database config

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,15 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
 
-  user: process.env.DB_USER,
-
-  password: process.env.DB_PASSWORD,
-
-  database: process.env.DB_NAME,
+const pool = mysql.createPool({
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT
 });
+
+module.exports = pool.promise();
 
 db.connect((err) => {
   if (err) {
