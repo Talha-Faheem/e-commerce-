c5c5655660e0a796564f0ea701fb4ad538c48a8c
Talha-Faheem <talhafaheem1103@gmail.com>
2026-06-27 00:38:28 +0500

    config

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 app.use( cors({ origin: ["http://localhost:5173", "http://localhost:5174"], credentials: true, }), );
 
 app.listen(
-  3000,
+  10000,
 
   () => {
     console.log(`Server Running on Port `);
